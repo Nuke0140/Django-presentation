9e6bd11123fbd0096c14523a3da7a266f151a437
Nilesh <nilshinde74@gmail.com>
2022-02-15 13:36:41 +0530

    initial commit

---
+++ hello/home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from home.models import Contact  #import our model from here 
+
+# Register your models here.
+
+admin.site.register(Contact) #register our model here 
---
+++ hello/home/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HomeConfig(AppConfig):#this our apps name 
+    name = 'home'
